8c17e0e442cc1f6edc4dc08fb3536d85aedac0dd
justin <walrathjaw@gmail.com>
2014-10-15 08:15:13 -0500

    Fixed broken tests.

--- test/utilities/TestImageUtils.py
+++ test/utilities/TestImageUtils.py
@@ -4,13 +4,12 @@ from pyplatformerengine.utilities.ImageUtils import ImageUtils
 
 class TestImageUtils(unittest.TestCase):
     def setUp(self):
-        pygame.init()
-        self.testImage = pygame.image.load("./BasicBall.png")
+        self.testImage = pygame.image.load("test/utilities/BasicBall.png")
         self.image = ImageUtils(self.testImage)
                 
     def testImageMethodsForManipulatingImages(self):
-        self.assertEqual(self.image.getImage, self.image.getImage, "Images are not the same")
-        self.assertNotEqual(self.image.getImage, self.image.scale(2,2), "Images are the same (scale)")
-        self.assertNotEqual(self.image.getImage, self.image.rotate(20), "Images are the same (rotate)")
-        self.assertNotEqual(self.image.getImage, self.image.flipVertical, "Images are the same (flipVertical)")
-        self.assertNotEqual(self.image.getImage, self.image.flipHorizontal, "Images are the same (flipHorizontal)")
+        self.assertEqual(self.image.getImage(), self.image.getImage(), "Images are not the same")
+        self.assertNotEqual(self.image.getImage(), self.image.scale(2,2).getImage(), "Images are the same (scale)")
+        self.assertNotEqual(self.image.getImage(), self.image.rotate(20).getImage(), "Images are the same (rotate)")
+        self.assertNotEqual(self.image.getImage(), self.image.flipVertical().getImage(), "Images are the same (flipVertical)")
+        self.assertNotEqual(self.image.getImage(), self.image.flipHorizontal().getImage(), "Images are the same (flipHorizontal)")
--- test/utilities/TestSettings.py
+++ test/utilities/TestSettings.py
@@ -3,15 +3,15 @@ from pyplatformerengine.utilities.Settings import Settings
 
 class TestSettings(unittest.TestCase):
     def setUp(self):
-        self.settings = Settings("./settings.conf")
+        self.settings = Settings("test/utilities/settings.conf")
         
     def testFetchSettingReturnsValue(self):
         self.assertEqual("800", self.settings.fetchSetting("screenWidth"), "screenWidth does not equal 800")
         self.assertEqual("600", self.settings.fetchSetting("screenHeight"), "screenHeight does not equal 600")
         self.assertEqual("True", self.settings.fetchSetting("debug"), "debug does not equal True")
-        self.assertTrue(self.settings.changeSetting("screenWidth","1280"), "screnWidth could not be changed to 1280")
-        self.assertTrue(self.settings.changeSetting("screenHeight","720"), "screenHeight could not be changed to 720")
-        self.assertTrue(self.settings.changeSetting("debug","False"), "debug could not be changed to False")
+        self.settings.changeSetting("screenWidth","1280")
+        self.settings.changeSetting("screenHeight","720")
+        self.settings.changeSetting("debug","False")
         self.assertEqual("1280", self.settings.fetchSetting("screenWidth"), "screenWidth does not equal 1280")
         self.assertEqual("720", self.settings.fetchSetting("screenHeight"), "screenHeight does not equal 720")
         self.assertEqual("False", self.settings.fetchSetting("debug"), "debug does not equal False")
